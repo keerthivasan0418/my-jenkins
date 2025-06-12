2c37acb2c11bda5849602898a12a83a27dd1e5c3
keerthivasan0418 <keerthivasanbmbk@gmail.com>
2025-06-12 22:07:34 +0530

    sodp previlages added

--- build-and-push.sh
+++ build-and-push.sh
@@ -19,20 +19,20 @@ docker login -u="keerthivasan041803" -p="kee@77189"
 
 
 echo "building the immage "
-docker build -t $image_name:$tag .
+sudo docker build -t $image_name:$tag .
 
 
 
 if [ $branch == "dev" ] then 
     echo "tagging the image $main_name"
-    docker tag $image_name:$tag $full_name
+    sudo docker tag $image_name:$tag $full_name
     echo "pushing the docker image"
-    docker push $main_name
+    sudo docker push $main_name
 fi
 
 if [$branch =="master"] then 
     echo "tagging the image $main_name_pri"
-    docker tag $image_name:$tag $full_name_pri  
+    sudo docker tag $image_name:$tag $full_name_pri  
     echo "pushing the image to pri repo"
-    docker push $main_name_pri 
+    sudo docker push $main_name_pri 
 fi 
