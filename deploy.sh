86b762bbf085fa63ab28e8a0c560acc278aada12
keerthivasan0418 <keerthivasanbmbk@gmail.com>
2025-06-16 20:34:38 +0530

    port changed

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@
 branch=$1
 if [ $branch == "dev" ]; then
     echo "running the docker container"
-    docker run -d -p 8080:8080 keerthivasan041803/dev:latest
+    docker run -d -p 80:80 keerthivasan041803/dev:latest
 fi
 
 
 if [ $branch == "prod" ]; then
     echo "running the docker container"
-    docker run -d -p 8080:8080 keerthivasan041803/prod:latest
+    docker run -d -p 80:80 keerthivasan041803/prod:latest
 fi
